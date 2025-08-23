cddd6b17c11226d4d08059e5fb6ad4519d9b43a7
temi <113118740+Temiawaye@users.noreply.github.com>
2025-08-23 14:24:57 +0100

    Landingpage/about

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/component/navbar"
+import { Settings } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -36,6 +37,124 @@ export default function Home() {
         </div>
 
       </div>
+
+      {/* About Section */}
+      <div className=" text-center ">
+        <h2 className="mt-10 text-lg font-bold text-[#a8a8ab]">ABOUT</h2>
+        <p className="text-black text-xl font-bold mt-5 ">Discover the Future of Online Shopping <br/> A Marketplace Built for You </p>
+        <p className="text-md font-medium text-[#8d8d90e7] mb-10" >Step Into the Next Level Marketplace - where innovation meets shopping.</p>
+
+        <div className="my-5 mx-25 text-black flex flex-col md:flex-row gap-10 lg:gap-15">
+        <div className="bg-[#e5e5e8] rounded-2xl  flex flex-col items-center pb-3 hover:scale-110">
+          <div>
+            <img src="https://placehold.co/250x250" alt="" />
+          </div>
+
+          <div className="bg-[#A2A3B1] justify-items-start pl-3 w-full">
+            <p>01</p>
+          </div>
+          
+          <div className="mx-3">
+            <p>Custom SEO Services</p>
+            <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          
+        </div>
+        <div className="bg-[#e5e5e8] rounded-2xl  flex flex-col items-center pb-3 hover:scale-110">
+          <div>
+            <img src="https://placehold.co/250x250" alt="" />
+          </div>
+
+          <div className="bg-[#A2A3B1] justify-items-start pl-3 w-full">
+            <p>02</p>
+          </div>
+          
+          <div className="mx-3">
+            <p>Custom SEO Services</p>
+            <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          
+        </div>
+        <div className="bg-[#e5e5e8] rounded-2xl  flex flex-col items-center pb-3 hover:scale-110">
+          <div>
+            <img src="https://placehold.co/250x250" alt="" />
+          </div>
+
+          <div className="bg-[#A2A3B1] justify-items-start pl-3 w-full">
+            <p>03</p>
+          </div>
+          
+          <div className="mx-3">
+            <p>Custom SEO Services</p>
+            <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          
+        </div>
+        </div>
+
+      </div>
+
+      {/* Features*/}
+      <div className="text-black text-center mt-10">
+       <h2 className="font-bold text-xl">Our Primary Features</h2>
+       <p className="text-sm px-3 text-[#8d8d90e7]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore<br/> culpa saepe laudantium,  ex illum? Deserunt ex maxime excepturi</p>
+
+       <div className="flex flex-col md:flex-row md:flex-wrap gap-10 justify-center m-5 px-20">
+
+        <div className=" flex flex-col items-center w-72 ">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+
+        <div className=" flex flex-col items-center w-72 ">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+
+        <div className=" flex flex-col items-center w-72">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+
+        <div className=" flex flex-col items-center w-72">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+
+        <div className=" flex flex-col items-center w-72">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+        
+        <div className=" flex flex-col items-center w-72">
+          <Settings className="mb-3" size={60} />
+          <p className="font-semibold text-xl">Effiency</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis deserunt</p>
+        </div>
+        
+       </div>
+
+      </div>
+
+      {/* Gallery */}
+      <div>
+        <h2 className="text-black text-3xl font-bold text-center my-10">Gallery</h2>
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-1 mx-10">
+          <img src="https://placehold.co/350x250" alt="" className="md:row-span-2 md:h-[500px]" />
+          <img src="https://placehold.co/350x250" alt="" className="md:col-span-2 md:" />
+          <img src="https://placehold.co/350x250" alt="" className="" />
+          <img src="https://placehold.co/350x250" alt="" className="md:row-span-2 md:h-[500px]" />
+          <img src="https://placehold.co/350x250" alt="" className="" />
+          <img src="https://placehold.co/350x250" alt="" className="" />
+        </div>
+      </div>
+
+      
 
     </div>
   );
